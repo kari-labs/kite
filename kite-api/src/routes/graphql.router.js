0fb7160164e3baff32cf4b0438a56b62e0f39e51
Ian Fabs <ianfabs@outlook.com>
2019-01-10 13:10:58 -0500

    Added graphqlUploadExpressMiddleware
    Added and implemented graphqlUploadExpressMiddleware with filesize limit of one gigabyte

--- kite-api/src/routes/graphql.router.js
+++ kite-api/src/routes/graphql.router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const graphqlHTTP = require('express-graphql');
+const {graphqlUploadExpress} = require('graphql-upload');
 const { buildSchema } = require('graphql');
 
 const { queryType } = require('../schemas/query.schema');
@@ -21,10 +22,17 @@ const root = {};
 Object.assign(root, ContainerResolvers);
 Object.assign(root, FilesystemResolvers);
 
-router.use('/', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-}));
+router.use(
+    '/', 
+    graphqlUploadExpress({ 
+        maxFileSize: 1000000000,
+        maxFiles: 10 
+    }), 
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    })
+);
 
 module.exports = router;
