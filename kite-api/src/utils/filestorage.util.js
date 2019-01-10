205ab51ab1719f6c78be064e6ba04d50d0c49487
Ian Fabs <ianfabs@outlook.com>
2019-01-10 13:15:55 -0500

    Added ancillary utility functions for uploading files

--- kite-api/src/utils/filestorage.util.js
+++ kite-api/src/utils/filestorage.util.js
@@ -1,6 +1,9 @@
-const fs = require('fs').promises;
+const fs = require('fs');
+const fsp = fs.promises;
 const path = require('path');
 const config = require('../../config/config');
+
+const isDir = async filepath => (await fsp.stat(filepath)).isDirectory();
 
 const getFileInfo = async (userid, filepath) => {
   const file = path.join(config.userFolderPath, userid, filepath);
@@ -10,7 +13,7 @@ const getFileInfo = async (userid, filepath) => {
     throw new Error('Access denied');
   }
   try {
-    const fileStats = await fs.stat(file)
+    const fileStats = await fsp.stat(file)
     return {
       name: path.basename(file),
       created: fileStats.birthtime,
@@ -37,11 +40,11 @@ const getDirContents = async (userid, filepath) => {
   }
   try {
     try {
-      const files = await fs.readdir(file)
+      const files = await fsp.readdir(file)
       return {
           files: await Promise.all(files.map(async filename => {
           const filepath = path.join(file, filename);
-          const fileStats =  await fs.stat(filepath);
+          const fileStats =  await fsp.stat(filepath);
           return {
             name: filename,
             created: fileStats.birthtime,
@@ -67,7 +70,7 @@ const getDirContents = async (userid, filepath) => {
 const getFileSizeInBytes = async userPath => {
   let absPath = path.join(config.userFolderPath, userPath);
   try {
-    const stats = await fs.stat(absPath);
+    const stats = await fsp.stat(absPath);
     return stats['size'];
   } catch (err) {
     if (err.code === 'ENOENT') {
@@ -79,12 +82,12 @@ const getFileSizeInBytes = async userPath => {
 };
 
 const getDirSizeInBytes = async (dirPath, memo) => {
-  const fileStats = await fs.stat(dirPath);
+  const fileStats = await fsp.stat(dirPath);
   if(fileStats.isDirectory()) {
-    const files = await fs.readdir(dirPath);
+    const files = await fsp.readdir(dirPath);
     for (let file of files) {
       const filePath = path.join(dirPath, file);
-      const stats = await fs.stat(filePath);
+      const stats = await fsp.stat(filePath);
       if(stats.isDirectory()) {
         await getDirSizeInBytes(filePath, memo);
       } else {
@@ -104,4 +107,34 @@ const getDirSizeInBytesHandler = async (userPath) => {
   return size;
 }
 
-module.exports = { getFileInfo, getDirContents, getFileSizeInBytes, getDirSizeInBytesHandler };
+const storeFS = (userid, { stream, filename }) => {
+  const UPLOAD_DIR = path.join(config.userFolderPath, userid);
+  const fullpath = `${UPLOAD_DIR}/${filename}`
+  return new Promise((resolve, reject) =>
+    stream
+      .on('error', error => {
+        if (stream.truncated)
+          // Delete the truncated file.
+          fs.unlinkSync(fullpath)
+        reject(error)
+      })
+      .pipe(fs.createWriteStream(fullpath))
+      .on('error', error => reject(error))
+      .on('finish', () => resolve({ fullpath }))
+  )
+}
+
+const processUpload = async (userid, upload) => {
+  const { createReadStream, filename, mimetype } = await upload
+  const stream = createReadStream()
+  const { fullpath } = await storeFS(userid, { stream, filename })
+  console.log(fullpath)
+  return {
+    name: filename,
+    size: await getDirSizeInBytesHandler(fullpath),
+    isdirectory: await isDir(fullpath)
+  };
+}
+
+
+module.exports = { getFileInfo, getDirContents, getFileSizeInBytes, getDirSizeInBytesHandler, processUpload };
