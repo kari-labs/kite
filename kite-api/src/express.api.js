7ac345b4cd13f97493d1066ba48d997aa9d59e0c
Matthew Conway <mattconway55@gmail.com>
2019-01-18 11:26:01 -0500

    Setup static hosting of production Vue build

--- kite-api/src/express.api.js
+++ kite-api/src/express.api.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -7,6 +8,7 @@ const port = 3000;
 const docker = require('./routes/docker.router');
 const graphql = require('./routes/graphql.router');
 
+app.use('/', cors(), express.static(path.join(__dirname, '../dist')));
 app.use('/api/docker', cors(), docker);
 app.use('/api/graphql', cors(), graphql);
 
---
+++ kite-web-element/vue.config.js
@@ -0,0 +1,5 @@
+const path = require("path");
+
+module.exports = {
+  outputDir: path.join(__dirname, "../kite-api/dist")
+};
