71a522a9acb52d6b7204cf10b5bfff9ce842a449
Ian Fabs <ianfabs@outlook.com>
2019-01-19 18:55:58 -0500

    Added router.js

---
+++ kite-web-element/src/router.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "@/views/Login.vue";
+import Containers from "@/views/Containers.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/containers",
+      name: "containers",
+      // route level code-splitting
+      // this generates a separate chunk (containers.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      //component: () => import(/* webpackChunkName: "containers" */ "./views/Containers.vue")
+      component: Containers
+    }
+  ]
+});
