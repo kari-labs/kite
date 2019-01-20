f431e1cd2b83fc7455fd488542919de181cc528c
Ian Fabs <ianfabs@outlook.com>
2019-01-19 19:05:06 -0500

    Better vue-router example

--- kite-web-element/src/App.vue
+++ kite-web-element/src/App.vue
@@ -1,7 +1,12 @@
 <template>
+  <!-- eslint-disable -->
   <div id="app">
-    <!-- eslint-disable -->
-    <router-view />
+    <nav>
+      <router-link to="/">login</router-link> | <router-link to="/containers">containers</router-link>
+    </nav>
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
@@ -20,5 +25,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+main {
+  margin: 2rem 0.5rem;
 }
 </style>
