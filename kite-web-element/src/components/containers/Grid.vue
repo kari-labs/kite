d8c2fcedc296e60d762a1f43e5637dc20e08ecca
Ian Fabs <ianfabs@outlook.com>
2019-01-23 13:27:05 -0500

    Added Grid.vue

---
+++ kite-web-element/src/components/containers/Grid.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container-grid">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "k-grid",
+    props: {
+        cardWidth: {
+            type: String,
+            default: "300px"
+        },
+        justifyContent: {
+            type: String,
+            default: "space-around"
+        },
+        rowGap: {
+            type: String,
+            default: "2rem"
+        },
+        columnGap: {
+            type: String,
+            default: "1rem"
+        }
+    },
+    mounted(){
+        let s = this.$el.style;
+        s["display"] = "grid";
+        s["grid-template-columns"] = "repeat(auto-fill, "+this.cardWidth+")";
+        s["grid-row-gap"] = this.rowGap;
+        s["grid-column-gap"] = this.columnGap;
+        s["justify-content"] = this.justifyContent;
+        s["width"] = "100%";
+    }
+}
+</script>
