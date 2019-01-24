fd58cb2eca6ce5479d66f2da6e399fef82dd4d9d
Matthew Conway <mattconway55@gmail.com>
2019-01-24 13:44:44 -0500

    Testing Linting Warnings

--- kite-api/src/routes/docker.router.js
+++ kite-api/src/routes/docker.router.js
@@ -10,10 +10,6 @@ const _404 = {
     error: 'Resource not found.',
     message: 'The requested container instance no longer exists on our server'
 }
-const _405 = {
-    error: 'Testing',
-    message: 'Testing Again'
-}
 const _500 = {
     error: 'Server Error',
     message: 'Server failed to spin up docker container'
