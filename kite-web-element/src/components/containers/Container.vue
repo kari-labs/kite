c4e14ed31f8492982729f6bda8156acc8284e7d3
Ian Fabs <ianfabs@outlook.com>
2019-01-25 13:29:15 -0500

    Updated Container.vue
    Updated styles
    Added status dot
    Added `container` property , to map container object to card
    Added card body slot because we haven't worked decided on card body totally

--- kite-web-element/src/components/containers/Container.vue
+++ kite-web-element/src/components/containers/Container.vue
@@ -1,18 +1,57 @@
 <template>
   <el-card>
-    <div slot="header" class="clearfix">
-      <span>Card name</span>
-      <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button>
+    <div
+      slot="header"
+      class="el-card-header"
+    >
+      <el-badge
+        is-dot
+        :type="container.status == 'running' ? 'success' : 'danger'"
+      >
+        <span class="el-card-header-title">
+          {{ container.name || "Card title" }}
+        </span>
+      </el-badge>
     </div>
-    <div v-for="o in 4" :key="o" class="text item">
-      {{'List item ' + o }}
-    </div>
+    <section class="el-card-body">
+      <slot>
+        We still haven't designed the cards body.
+      </slot>
+    </section>
   </el-card>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "KOntainer",
+  props: {
+    container: {
+      type: Object,
+      default: ()=>({
+        owner: "",
+        container_id: "",
+        status: "running",
+        image: "",
+        name: ""
+      })
+    }
+  },
+
+};
 </script>
 
-<style>
+<style scoped>
+.el-card-header{
+  display: grid;
+  text-align: left;
+}
+.el-card-header-title{
+  margin: 0px;
+  font-weight: 500;
+}
+.el-card-body{
+  display: flex;
+  text-align: left;
+  font-weight: 300;
+}
 </style>
