f45785e6707ef760185ecb743f7a9ea94fc81b97
Ian Fabs <ianfabs@outlook.com>
2019-01-25 13:27:02 -0500

    Updated SideNav.vue
    Linted.
    Changed container icon because of a weird spacing bug.
    Added accessibility features to v-to directive.

--- kite-web-element/src/components/SideNav.vue
+++ kite-web-element/src/components/SideNav.vue
@@ -1,75 +1,116 @@
 <template>
   <el-menu
-    default-active="1"
+    default-active="0"
     class="el-aside"
     @open="handleOpen"
     @close="handleClose"
     :collapse="toggle"
   >
-    <el-menu-item index="1" v-to="'containers'">
-      <fa-icon icon="cube" size="lg" class="mr-1"/>
-      <span slot="title">Containers</span>
+    <el-menu-item
+      index="1"
+      v-to="'containers'"
+    >
+      <fa-icon
+        icon="dot-circle"
+        size="lg"
+        class="mr-1"
+      />
+      <span slot="title">
+        Containers
+      </span>
     </el-menu-item>
-    <el-menu-item index="2" v-to="'editor'" disabled aria-disabled="true">
-      <fa-icon icon="file" size="lg" class="mr-1"/>
-      <span slot="title">Editor</span>
+    <el-menu-item
+      index="2"
+      v-to="'editor'"
+      disabled
+      aria-disabled="true"
+    >
+      <fa-icon
+        icon="file"
+        size="lg"
+        class="mr-1"
+      />
+      <span slot="title">
+        Editor
+      </span>
     </el-menu-item>
     <el-submenu index="3">
       <template slot="title">
-        <fa-icon icon="cog" size="lg" class="mr-1"/>
-        <span slot="title">Settings</span>
+        <fa-icon
+          icon="cog"
+          size="lg"
+          class="mr-1"
+        />
+        <span slot="title">
+          Settings
+        </span>
       </template>
       <el-menu-item-group>
-        <span slot="title">Settings</span>
-        <el-menu-item index="1-1">Dark Theme
-          <el-switch v-model="dark" class="ml-4"></el-switch>
+        <span slot="title">
+          Settings
+        </span>
+        <el-menu-item index="1-1">
+          Dark Theme
+          <el-switch
+            v-model="dark"
+            class="ml-4"
+          />
         </el-menu-item>
       </el-menu-item-group>
     </el-submenu>
     <el-menu-item index="4">
-      <fa-icon icon="sign-out-alt" size="lg" class="mr-1"/>
-      <span slot="title">Logout</span>
+      <fa-icon
+        icon="sign-out-alt"
+        size="lg"
+        class="mr-1"
+      />
+      <span slot="title">
+        Logout
+      </span>
     </el-menu-item>
   </el-menu>
 </template>
 
 <script>
 export default {
-  name: "k-nav",
-  props: ["toggle"],
+  name: "KNav",
+  props: {
+    toggle: Boolean
+  },
   data() {
     return {
       dark: false
     };
   },
   methods: {
-    handleOpen(event) {
+    handleOpen() {
       //Code for nav opening
     },
-    handleClose(event) {
+    handleClose() {
       //code for nav closing
     }
   },
   directives: {
     to: {
-      inserted: (el, bin, vnode) =>{
-        el.tabindex="1";
-        el.addEventListener("click", (event)=>{
-          try{
-            this.$router.push(bin.value);
-          }catch(error){
+      inserted: (el, bin, vnode) => {
+        el.tabindex = "1";
+        el.attributes[0].value="button"
+        const handleButton = () => {
+          try {
+            vnode.componentInstance.$router.push(bin.value);
+          } catch (error) {
+            // eslint-disable-next-line
             console.group("v-to");
+            // eslint-disable-next-line
             console.log(error);
+            // eslint-disable-next-line
             console.groupEnd("v-to");
           }
-        });
+        };
+        el.addEventListener("click", handleButton);
+        el.addEventListener("keypress", handleButton);
       }
     }
   }
 };
 </script>
-
-<style>
-.el-aside {
-}
-</style>
