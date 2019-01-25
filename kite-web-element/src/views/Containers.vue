7ccefff4619806aff0a7549b96c69849efe664f1
Ian Fabs <ianfabs@outlook.com>
2019-01-25 13:34:13 -0500

    Updated Containers.vue
    Made example better

--- kite-web-element/src/views/Containers.vue
+++ kite-web-element/src/views/Containers.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
     <k-grid>
-      <el-card></el-card>
-      <el-card></el-card>
-      <el-card></el-card>
-      <el-card></el-card>
+      <k-ontainer 
+        v-for="o in 4" 
+        :key="o"
+      />
     </k-grid>
   </div>
 </template>
 
 <script>
 import KGrid from "@/components/containers/Grid.vue";
+import KOntainer from "@/components/containers/Container.vue";
 
 export default {
   components: {
-    KGrid
+    KGrid,
+    KOntainer
   }
 };
 </script>
