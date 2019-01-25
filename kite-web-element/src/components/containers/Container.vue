a8b43443a621c117be3511da009b160c5baa02c8
Ian Fabs <ianfabs@outlook.com>
2019-01-24 20:02:19 -0500

    Added Container.md

---
+++ kite-web-element/src/components/containers/Container.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-card>
+    <div slot="header" class="clearfix">
+      <span>Card name</span>
+      <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button>
+    </div>
+    <div v-for="o in 4" :key="o" class="text item">
+      {{'List item ' + o }}
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
