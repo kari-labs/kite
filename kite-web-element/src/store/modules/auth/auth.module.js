b7d3207dd49446c1ca289a24d84d8214c4e608d9
RetWolf <mattconway55@gmail.com>
2019-02-12 14:14:27 -0500

    Updated auth on front-end

--- kite-web-element/src/store/modules/auth/auth.module.js
+++ kite-web-element/src/store/modules/auth/auth.module.js
@@ -1,5 +1,7 @@
 import { loginUser, signOutUser } from '@/utils/auth.util';
+import router from '@/router.js';
 import * as types from './auth.types';
+import { Message } from 'element-ui';
 
 export const authModule = {
   state: {
@@ -14,7 +16,7 @@ export const authModule = {
     }
   },
   actions: {
-    [types.LOGIN_USER] ({ commit }, { userid, pass, component }) {
+    [types.LOGIN_USER] ({ commit }, { userid, pass, redirect }) {
       const response = loginUser(userid, pass);
       
       response.then(result => {
@@ -23,33 +25,33 @@ export const authModule = {
             type: types.STORE_USER,
             user: result.data.user
           });
-          if(component.$route.query.redirect) component.$router.push(component.$route.query.redirect);
+          if(redirect) router.push(redirect);
           // We give the users the choice to chose a homepage and send them there by default
-          else component.$router.push('/containers');
+          else router.push('/containers');
         } else {
           if(result.errors) {
             result.errors.map(err => {
-              component.$message.error(err.message);
+              Message.error(err.message);
             });
           }
         }
       });
     },
-    [types.SIGN_OUT_USER] ({ commit }, { component }) {
+    [types.SIGN_OUT_USER] ({ commit }) {
       const response = signOutUser();
 
       response.then(result => {
         if(result.data.stats !== null) {
           if(result.data.status === 'Successfully signed out.') {
             commit(types.REMOVE_USER);
-            component.$router.push('/');
+            router.push('/');
           } else {
-            component.$message.error(result.data.status);
+            Message.error(result.data.status);
           }
         } else {
           if(result.errors) {
             result.errors.map(err => {
-              component.$message.error(err.message);
+              Message.error(err.message);
             });
           }
         }
