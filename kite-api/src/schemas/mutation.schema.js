a5579ace17a512477e3a912cd520ef7ecdb6e3be
Ian Fabs <ianfabs@outlook.com>
2019-02-13 21:22:51 -0500

    Updated mutation.schema

--- kite-api/src/schemas/mutation.schema.js
+++ kite-api/src/schemas/mutation.schema.js
@@ -2,6 +2,8 @@ const mutationType = `
     type Mutation {
         createContainer(nickname: String!): Container
         deleteContainer(_id: String!): String
+        deleteAllContainers: Int
+        clearFalseContainersFromUser: Boolean
         singleUpload(userid: String!, file: Upload!): File!
         multipleUpload(userid: String!, files: [Upload!]!): [File!]!
         createUser(userid: String!, password: String!, name: String! scope: [String!]): User
