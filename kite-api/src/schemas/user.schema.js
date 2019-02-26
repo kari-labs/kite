1bc1d87397745411b47070d138e23c7d805d5ce9
Kyle Riley <KMRiley4136@gmail.com>
2019-02-26 13:43:55 -0500

    fixed bug with containers not populating when updating users

--- kite-api/src/schemas/user.schema.js
+++ kite-api/src/schemas/user.schema.js
@@ -9,7 +9,7 @@ const userType = `
     forceReset: Boolean,
     logins: Int,
     name: String,
-    containers: [String],
+    containers: [Container],
     preferences: PreferencesConfig,
     scope: [String]
   }
