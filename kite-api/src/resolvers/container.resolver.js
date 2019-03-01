e49724404f076b35e28914d16f0244f4d509b7d2
Kyle <Arow411@gmail.com>
2019-02-28 21:29:58 -0500

    Add apollo

--- kite-api/src/resolvers/container.resolver.js
+++ kite-api/src/resolvers/container.resolver.js
@@ -1,78 +1,99 @@
 const { createContainer, deleteContainer, getContainer, getContainers, deleteAllContainers, restoreContainer } = require('../utils/container.util');
+const { pubsub, UPDATE_CONTAINER } = require('./pubsub');
+const { withFilter } = require('graphql-subscriptions');
 
 const ContainerResolvers = {
-  createContainer: async ({ nickname }, req) => {
-    let containers;
-    try {
-      let user = req.session.userStore;
-      if(user) {
-        containers = await createContainer({ owner: user._id, userid: user.userid, nickname });
-      }
-      else throw new Error("User not logged in");
-      return containers;
-    } catch (err) {
-      console.error(err);
-      throw new Error(err);
-    }
-  },
-  getContainer: async ({ container_id }, req) => {
-    let containers;
-    try {
-      const user = req.session.userStore;
-      if(user) containers = await getContainer(container_id);
-      else throw new Error("User not logged in");
-      return containers;
-    } catch (err) {
-      throw err;
-    }
-  },
-  getContainers: async (_, req) => {
-    let containers;
-    try {
-      let user = req.session.userStore;
-      if(user) containers = await getContainers(user._id);
-      else throw new Error("User not logged in");
-      return containers;
-    } 
-    catch (err) {
-      throw err;
-    }
-  },
-  deleteContainer: async ({ _id, permanently = false }, req) => {
-    console.log(permanently);
-    try {
-      let user = req.session.userStore;
-      if(user) {
-        await deleteContainer(_id, user._id, permanently);
-      }
-      else throw new Error("User not logged in");
-      return `Successfully deleted container ${_id}`;
-    } catch (err) {
-      console.log(err);
-      throw new Error(err);
-    }
-  },
-  deleteAllContainers: async (_, req) => {
-    try {
-      let user = req.session.userStore;
-      if(user) {
-        let containers = await deleteAllContainers(user._id);
+  Mutation: {
+    createContainer: async (root, { nickname }, req) => {
+      let containers;
+      try {
+        let user = req.session.userStore;
+        if(user) {
+          containers = await createContainer({ owner: user._id, userid: user.userid, nickname });
+          pubsub.publish(UPDATE_CONTAINER, { container: containers, owner: user.userid });
+        }
+        else throw new Error("User not logged in");
         return containers;
+      } catch (err) {
+        console.error(err);
+        throw new Error(err);
       }
-      else throw new Error("User not logged in");
-    } catch (err) {
-      console.log(err);
-      throw new Error(err);
-    }
+    },
+    deleteContainer: async (root, { _id, permanently = false }, req) => {
+      console.log(permanently);
+      try {
+        let user = req.session.userStore;
+        if(user) {
+          await deleteContainer(_id, user._id, permanently);
+        }
+        else throw new Error("User not logged in");
+        return `Successfully deleted container ${_id}`;
+      } catch (err) {
+        console.log(err);
+        throw new Error(err);
+      }
+    },
+    deleteAllContainers: async (root, args, req) => {
+      try {
+        let user = req.session.userStore;
+        if(user) {
+          let containers = await deleteAllContainers(user._id);
+          return containers;
+        }
+        else throw new Error("User not logged in");
+      } catch (err) {
+        console.log(err);
+        throw new Error(err);
+      }
+    },
+    restoreContainer: async (root, {_id}, req) => {
+      if(req.session.userStore) {
+        await restoreContainer(_id);
+        return true;
+      }else {
+        throw new Error("User not logged in");
+      }
+    },
   },
-  restoreContainer: async ({_id}, req) => {
-    if(req.session.userStore) {
-      await restoreContainer(_id);
-      return true;
-    }else {
-      throw new Error("User not logged in");
-    }
+  Query: {
+    getContainer: async (root, { container_id }, req) => {
+      let containers;
+      try {
+        const user = req.session.userStore;
+        if(user) containers = await getContainer(container_id);
+        else throw new Error("User not logged in");
+        return containers;
+      } catch (err) {
+        throw err;
+      }
+    },
+    getContainers: async (root, args, req) => {
+      let containers;
+      try {
+        let user = req.session.userStore;
+        if(user) containers = await getContainers(user._id);
+        else throw new Error("User not logged in");
+        return containers;
+      } 
+      catch (err) {
+        throw err;
+      }
+    },
   },
+  Subscription: {
+    updateContainer: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(UPDATE_CONTAINER),
+        (payload, {userid}) => {
+          console.log('subscription pl', payload)
+          return payload.owner == userid;
+        },
+      ),
+      resolve: (payload) => {
+        return payload.container
+      }
+    }
+  }
 };
 
 module.exports = { ContainerResolvers };
--- kite-api/src/resolvers/user.resolver.js
+++ kite-api/src/resolvers/user.resolver.js
@@ -9,59 +9,64 @@ const {
 } = require('../utils/user.util');
 
 const UserResolvers = {
-  createUser: async ({ ...userData }) => {
-    try {
-      const userDB = createUser(userData);
-      return {
-        ...userDB.toObject(),
-        _id: userDB._id.toString(),
+  Query: {
+    getUser: async (root, args, req) => {
+      try {
+        const scope = await getUser(req);
+        return scope;
+      } catch (err) {
+        throw new Error(err);
       }
-    } catch(err) {
-      throw new Error(err);
-    }
-  },
-  deleteUser: async ({ ...userData}) =>{
-    try{
-      return deleteUser(userData);
-    } catch (err) {
-      throw new Error (err);
-    }
-  },
-  loginUser: async ({ ...userData }, req) => {
-    const userDB = await loginUser(userData, req);
-    if(typeof userDB === "object") {
-      return {
-        ...userDB.toObject(),
-        _id: userDB._id.toString()
+    },
+    getUsers: async () => {
+      try {
+        const users = await getUsers();
+        return users;
+      } catch (err) {
+        throw new Error(err);
       }
-    }
-    else throw new Error(userDB);
+    },
   },
-  getUser: async (_, req) => {
-    try {
-      const scope = await getUser(req);
-      return scope;
-    } catch (err) {
-      throw new Error(err);
-    }
+  Mutation: {
+    createUser: async (root, { ...userData }) => {
+      try {
+        const userDB = createUser(userData);
+        return {
+          ...userDB.toObject(),
+          _id: userDB._id.toString(),
+        }
+      } catch(err) {
+        throw new Error(err);
+      }
+    },
+    deleteUser: async (root, { ...userData}) =>{
+      try{
+        return deleteUser(userData);
+      } catch (err) {
+        throw new Error (err);
+      }
+    },
+    loginUser: async (root, { ...userData }, req) => {
+      const userDB = await loginUser(userData, req);
+      if(typeof userDB === "object") {
+        return {
+          ...userDB.toObject(),
+          _id: userDB._id.toString()
+        }
+      }
+      else throw new Error(userDB);
+    },
+    signOutUser: async(root, args, req) => {
+      const status = await signOutUser(req);
+      if(status) return 'Successfully signed out.';
+      else return 'Error signing user out.'
+    },
+    updateUser: async(root, { userid: userIdToUpdate, user: userNewInfo }) => {
+      const updatedUser = updateUser(userIdToUpdate, userNewInfo);
+      return updatedUser;
+    },
   },
-  getUsers: async () => {
-    try {
-      const users = await getUsers();
-      return users;
-    } catch (err) {
-      throw new Error(err);
-    }
-  },
-  signOutUser: async(_, req) => {
-    const status = await signOutUser(req);
-    if(status) return 'Successfully signed out.';
-    else return 'Error signing user out.'
-  },
-  updateUser: async({ userid: userIdToUpdate, user: userNewInfo }) => {
-    const updatedUser = updateUser(userIdToUpdate, userNewInfo);
-    return updatedUser;
-  }
+  Subscription: {},
 }
 
 module.exports = { UserResolvers };
