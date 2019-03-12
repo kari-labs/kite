9b0504a06d39a605f25ed6ea3ba984d7fb5ed217
Ian Fabs <ianfabs@outlook.com>
2019-03-11 21:35:28 -0400

    Removed useless lines of code

--- kite-web-element/src/views/FileManager.vue
+++ kite-web-element/src/views/FileManager.vue
@@ -5,29 +5,11 @@
 </template>
 
 <script>
-import KEditableText from "@/components/filesys/KEditableText.vue"
 import KFileManager from "@/components/filesys/FileManager.vue";
 
 export default {
-  data(){
-    return {
-    };
-  },
   components: {
     KFileManager,
-    KEditableText,
   }
 };
 </script>
-
-<style>
-.el-icon {
-  margin-right: 5px;
-  vertical-align: baseline;
-  width: 18px;
-  text-align: center;
-  font-size: 18px;
-  line-height: 1;
-  display: inline-block;
-}
-</style>
